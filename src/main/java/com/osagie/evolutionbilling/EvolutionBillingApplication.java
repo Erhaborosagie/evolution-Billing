a9e33ff2cb647a2d09073b11388bc3fb9cd00677
Osagie Erhabor <erhaborosagie@gmail.com>
2024-03-21 21:26:22 +0100

    Initial Bootstrap

---
+++ src/main/java/com/osagie/evolutionbilling/EvolutionBillingApplication.java
@@ -0,0 +1,13 @@
+package com.osagie.evolutionbilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvolutionBillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvolutionBillingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osagie/evolutionbilling/EvolutionBillingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osagie.evolutionbilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvolutionBillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
